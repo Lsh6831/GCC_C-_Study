8d3dbe55f0e556a9aa21ef3ae0c800ddefb7f6aa
Lsh6831 <rltmsla5@naver.com>
2026-04-20 12:44:18 +0900

    feat : 표쥰(std) 네임스페이스 사용 선언 및 축약

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hellow World1" << std::endl;
-	std::cout << "Hellow World2\n";
-	std::cout << "Hellow World" << std::endl;
+	cout << "Hellow World1" << std::endl;
+	cout << "Hellow World2\n";
+	cout << "Hellow World" << std::endl;
 	return 0;
 }
