50ee82a27199671e10f4c6993058fc94e248245f
Lsh6831 <rltmsla5@naver.com>
2026-04-20 14:57:24 +0900

    feat : 자료형 변수 선언및 초기화, 변수에 저장된 데티어 출력, 메모리 크기 확인

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -1,18 +1,55 @@
 #include <iostream>
 using namespace std;
 
+
+char userName[50];
+char charactorClass[50];
+
 int main()
 {
-	char userName[50];
+	// 1. 다양한 자료형의 변수 선언 및 초기값 할당
+	int level = 1;
+	int hp = 100;
+	int mp = 100;
+	float attackspeed = 5.5f;
 
-	cout << "[User Name]\n";
+	//스탯 시스템
+	int strength = 50;
+	int dexterity = 50;
+	int vitallity = 50;
+	int energy = 50;
+
+	// 하드코어 모드 여부 변수
+	bool isHardcore = true;
+
+	cout << "[ Character Creation ]\n";
 	cout << "Input your name : ";
 	cin >> userName;
-
-	cout << "::::::::::Welcom to the D&D World::::::::::\n";
-	cout << "UserName : [" << userName << "]!\n";
-
+	cout << "Input your Class : ";
+	cin >> charactorClass;
 	
+	cout << "\n:::::::Welcom to the SCancuart ::::::\n";
+	cout << "user Name : {" << userName << "\n}";
+
+	//2. 변수에 저장된 데이터 출력
+	cout << "------------Character Status----------|n";
+	cout << "Class : " << charactorClass << "\n";
+	cout << "Level : " << level<<"\n";
+	cout << "HP : " << hp << "\n";
+	cout << "Attack Speed : " << attackspeed << "\n";
+	cout << "Strength : " << strength << "\n";
+	cout << "Dexterity : " << dexterity << "\n";
+	cout << "Vitalty : " << vitallity << "\n";
+	cout << "Energy : " << energy << "\n";
+	cout << "Hardcore Mode : " << isHardcore << "(1: true, 0: flase)\n";
+
+	// 3. siezof 연사자를 이용한 메모리 크기 확인
+	cout << "[Memory Check] int type size : " << sizeof(hp) << "bytes\n";
+	cout << "[Memory Check] vool type size : " << sizeof(isHardcore) << " bytes\n";
+
+
+
+
 	
 	return 0;
 }
