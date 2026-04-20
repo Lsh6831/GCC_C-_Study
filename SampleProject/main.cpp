fc9febb62ae549e4c09d821c1e5d10699f864fc5
Lsh6831 <rltmsla5@naver.com>
2026-04-20 14:17:30 +0900

    feat : cin 사용하여 입력받기 추가

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main()
 {
-	cout << "Hellow World1" << std::endl;
-	cout << "Hellow World2\n";
-	cout << "Hellow World" << std::endl;
+	char userName[50];
+
+	cout << "[User Name]\n";
+	cout << "Input your name : ";
+	cin >> userName;
+
+	cout << "::::::::::Welcom to the D&D World::::::::::\n";
+	cout << "UserName : [" << userName << "]!\n";
+
+	
+	
 	return 0;
 }
