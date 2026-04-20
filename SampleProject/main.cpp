756a9703a3455e5603d2b9e8b32353ea1447556e
Lsh6831 <rltmsla5@naver.com>
2026-04-20 15:21:34 +0900

    feat : 변수 사칙연산 추

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -7,11 +7,6 @@ char charactorClass[50];
 
 int main()
 {
-	// 1. 다양한 자료형의 변수 선언 및 초기값 할당
-	int level = 1;
-	int hp = 100;
-	int mp = 100;
-	float attackspeed = 5.5f;
 
 	//스탯 시스템
 	int strength = 50;
@@ -19,11 +14,23 @@ int main()
 	int vitallity = 50;
 	int energy = 50;
 
+	// 1. 다양한 자료형의 변수 선언 및 초기값 할당
+	int level = 1;
+	int hp = 100;
+	int mp = 100;
+	float attackSpeed = dexterity / 10.0;
+	float attckDamage = strength * 0.2f;
+	double movingSpeed = dexterity / 30.0;
+
+
+
 	//Ressist
 	int fireResist = 0;
 	int lightingRessist = 0;
 	int coldRessist = 0;
 	int poisonRessist = 0;
+
+
 
 
 	// 하드코어 모드 여부 변수
@@ -43,7 +50,9 @@ int main()
 	cout << "Class : " << charactorClass << "\n";
 	cout << "Level : " << level<<"\n";
 	cout << "HP : " << hp << "\n";
-	cout << "Attack Speed : " << attackspeed << "\n";
+	cout << "Attck Damage : " << attckDamage << "\n";
+	cout << "Attack Speed : " << attackSpeed << "\n";
+	cout << "MovingSpeed : " << movingSpeed << "\n";
 	cout << "Strength : " << strength << "\n";	
 	cout << "Dexterity : " << dexterity << "\n";
 	cout << "Vitalty : " << vitallity << "\n";
